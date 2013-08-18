20365de6b1a442cfb5fd8975d70f9ae25dcd4a38
Alex Grin <lyoshenka@gmail.com>
2013-08-18 15:26:27 -0700

    The commit message was empty

--- ddns.sh
+++ ddns.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
+# Step 1: Fill in EMAIL, TOKEN, DOMAIN and SUBDOMAIN
+# Step 2: Create an A record on Cloudflare with the subdomain you chose
+# Step 3: Run "./ddns.sh -l" to get the rec_id of the record you created. 
+#         Fill in REC_ID below
+# Step 4: Run "./ddns.sh". It should tell you that record was updated or that it didn't need updating.
+#         Use "./ddns.sh -s to silence normal output (e.g. to run as a cron script)"
+
 IP=`curl -s http://icanhazip.com`
 IP_FILE='/tmp/ddns_last_ip'
-LAST_IP=`cat $IP_FILE`
+[[ -r "$IP_FILE" ]] && LAST_IP=`cat $IP_FILE` || LAST_IP=''
 
 EMAIL=''
 TOKEN=''
 DOMAIN=''
 SUBDOMAIN=''
+REC_ID=''
+
+CURL="curl -s https://www.cloudflare.com/api_json.html -d email=$EMAIL -d tkn=$TOKEN -d z=$DOMAIN "
 
 if [ "$IP" == "$LAST_IP" ]; then
-  echo "IP Unchanged"
-else
-  curl https://www.cloudflare.com/api_json.html \
-    -d 'a=rec_edit' \
-    -d "email=$EMAIL" \
-    -d "tkn=$TOKEN" \
-    -d "z=$DOMAIN" \
-    -d 'id=9001' \
-    -d 'type=A' \
-    -d "name=$SUBDOMAIN" \
-    -d 'ttl=1' \
-    -d "content=$IP"
-  echo $IP > $IP_FILE
+  [ "$1" != "-s" ] && echo "IP Unchanged"
+  exit
 fi
+
+if [ "$1" == "-l" ]; then
+  $CURL -d 'a=rec_load_all' | sed -e 's/[{}]/\n/g' | grep '"name":"'"$SUBDOMAIN"'.'"$DOMAIN"'"' | grep '"type":"A"' | sed -e 's/,/\n/g'
+  exit
+fi
+
+[ "$1" != "-s" ] && echo "Setting IP to $IP"
+
+$CURL \
+  -d 'a=rec_edit' \
+  -d "id=$REC_ID" \
+  -d 'type=A' \
+  -d "name=$SUBDOMAIN" \
+  -d 'ttl=1' \
+  -d 'service_mode=0' \
+  -d "content=$IP" \
+  1>/dev/null
+
+echo $IP > $IP_FILE
