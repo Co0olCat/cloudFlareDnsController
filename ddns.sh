09e135b5fb909b5ee7e85098a2161f2d81dd8ecf
Alex Grin <alex@grin.io>
2016-07-24 10:07:23 -0400

    The commit message was empty

--- ddns.sh
+++ ddns.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# Step 1: Fill in EMAIL, TOKEN, DOMAIN and SUBDOMAIN. Your token is here: https://www.cloudflare.com/my-account
+# Step 1: Fill in EMAIL, TOKEN, DOMAIN and SUBDOMAIN. Your API token is here: https://www.cloudflare.com/a/account/my-account
+#         Make sure the token is the Global token, or has these permissions: #zone:read, #dns_record:read, #dns_records:edit
 # Step 2: Create an A record on Cloudflare with the subdomain you chose
-# Step 3: Run "./ddns.sh -l" to get the rec_id of the record you created. 
-#         Fill in REC_ID below
+# Step 3: Run "./ddns.sh -l" to get the zone_id and rec_id of the record you created.
+#         Fill in ZONE_ID and REC_ID below
+#         This step is optional, but will save you 2 requests every time you this script
 # Step 4: Run "./ddns.sh". It should tell you that record was updated or that it didn't need updating.
 # Step 5: Run it every hour with cron. Use the '-s' flag to silence normal output
 #         0 * * * * /path/to/ddns.sh -s
@@ -13,39 +15,46 @@ EMAIL=''
 TOKEN=''
 DOMAIN=''
 SUBDOMAIN=''
+ZONE_ID=''
 REC_ID=''
 
 
 
 set -euo pipefail
+#set -x # enable for debugging
 
-IP=$(curl -s http://ipv4.icanhazip.com)
-IP_FILE='/tmp/ddns_last_ip'
-[[ -r "$IP_FILE" ]] && LAST_IP=$(cat $IP_FILE) || LAST_IP=''
+VERBOSE="[ '${1:-}' != '-s' ]"
+LOOKUP="[ '${1:-}' == '-l' ]"
 
-CURL="curl -s https://www.cloudflare.com/api_json.html -d email=$EMAIL -d tkn=$TOKEN -d z=$DOMAIN "
+API_URL="https://api.cloudflare.com/client/v4"
+CURL="curl -s \
+  -H Content-Type:application/json \
+  -H X-Auth-Key:$TOKEN \
+  -H X-Auth-Email:$EMAIL "
 
-if [ "${1:-}" == "-l" ]; then
-  $CURL -d 'a=rec_load_all' | sed -e 's/[{}]/\n/g' | grep '"name":"'"$SUBDOMAIN"'.'"$DOMAIN"'"' | grep '"type":"A"' | sed -e 's/,/\n/g' | grep '\"rec_id\|\"name'
+
+if [ -z "$ZONE_ID" ] || $LOOKUP; then
+  ZONE_ID="$($CURL "$API_URL/zones?name=$DOMAIN" | sed -e 's/[{}]/\n/g' | grep '"name":"'"$DOMAIN"'"' | sed -e 's/,/\n/g' | grep '"id":"' | cut -d'"' -f4)"
+  $VERBOSE && echo "ZONE_ID='$ZONE_ID'"
+fi
+
+if [ -z "$REC_ID" ] || $LOOKUP; then
+  REC_ID="$($CURL "$API_URL/zones/$ZONE_ID/dns_records" | sed -e 's/[{}]/\n/g' | grep '"name":"'"$SUBDOMAIN"'.'"$DOMAIN"'"' | sed -e 's/,/\n/g' | grep '"id":"' | cut -d'"' -f4)"
+  $VERBOSE && echo "REC_ID='$REC_ID'"
+fi
+
+$LOOKUP && exit 0
+
+IP="$(curl -s http://ipv4.icanhazip.com)"
+RECORD_IP="$($CURL "$API_URL/zones/$ZONE_ID/dns_records/$REC_ID" | sed -e 's/[{}]/\n/g' | sed -e 's/,/\n/g' | grep '"content":"' | cut -d'"' -f4)"
+
+if [ "$IP" == "$RECORD_IP" ]; then
+  $VERBOSE && echo "IP Unchanged"
   exit 0
 fi
 
-if [ "$IP" == "$LAST_IP" ]; then
-  [ "${1:-}" != "-s" ] && echo "IP Unchanged"
-  exit 0
-fi
+$VERBOSE && echo "Setting IP to $IP"
 
-[ "${1:-}" != "-s" ] && echo "Setting IP to $IP"
+$CURL -X PUT "$API_URL/zones/$ZONE_ID/dns_records/$REC_ID" --data '{"type":"A","name":"'"$SUBDOMAIN"'","content":"'"$IP"'","proxied":false}' 1>/dev/null
 
-$CURL \
-  -d 'a=rec_edit' \
-  -d "id=$REC_ID" \
-  -d 'type=A' \
-  -d "name=$SUBDOMAIN" \
-  -d 'ttl=1' \
-  -d 'service_mode=0' \
-  -d "content=$IP" \
-  1>/dev/null
-  
-echo "$IP" > $IP_FILE
 exit 0
